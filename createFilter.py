62967f759196acda7094d5eb60c5b7d8106b4284
Alexander Stoklasa <astoklas@gmail.com>
2016-03-30 16:12:00 +0200

    change input to raw_input to omit traceback

--- createFilter.py
+++ createFilter.py
@@ -2,8 +2,8 @@ __author__ = 'astoklas'
 
 import requests
 
-name = input("Please enter name if the FILTER:")
-port = input("Please enter port to FILTER:")
+name = raw_input("Please enter name of the FILTER:")
+port = raw_input("Please enter port to FILTER:")
 
 url = "https://10.19.0.101:8443/controller/nb/v2/monitor/filter/%s" % name
 payload = "{\r\n  \"vlanPriority\": \"\",\r\n  \"tcpOptionLength\": \"\",\r\n  \"vlanId\": \"\",\r\n  \"name\": \"%s\",\r\n  \"tosBits\": \"\",\r\n  \"networkDst\": \"\",\r\n  \"transportPortDst\": \"%s\",\r\n  \"datalayerSrc\": \"\",\r\n  \"datalayerDst\": \"\",\r\n  \"httpMethodId\": \"\",\r\n  \"transportPortSrc\": \"\",\r\n  \"networkSrc\": \"\",\r\n  \"etherType\": \"0x0800\",\r\n  \"bidirectional\": \"false\",\r\n  \"protocol\": \"6\"\r\n}" % (name,port)
@@ -21,7 +21,7 @@ if response.status_code != 201:
     print("Something went wrong ... no time to fix this just exit")
     exit(1)
 
-user_input = input("Press Enter to Delete filter: ")
+user_input = raw_input("Press Enter to Delete filter: ")
 print("Delete FILTER")
 response = requests.request("DELETE", url, headers=headers, verify=False)
 
